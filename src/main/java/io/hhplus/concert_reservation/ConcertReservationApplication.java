6f68c3a8e2cbd5623c6d470d3d5867d1a5414831
lkk6201 <lkk6201@gmail.com>
2024-10-10 01:59:55 +0900

    Initial commit

---
+++ src/main/java/io/hhplus/concert_reservation/ConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.concert_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hhplus/concert_reservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.concert_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
